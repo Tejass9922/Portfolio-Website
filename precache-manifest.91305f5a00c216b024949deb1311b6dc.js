6c6941afd41ccc1372cb7583df9bccd3749c96ea
Tejas Sameera <samra.tejas5@gmail.com>
2020-04-30 15:30:09 -0400

    Updates

---
+++ precache-manifest.91305f5a00c216b024949deb1311b6dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a693d7a2dc4a598a86b6b132189b4af7",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "5dbead62dd3cf68be35a",
+    "url": "/Portfolio-Website/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "b603c3bdab12a979d10b",
+    "url": "/Portfolio-Website/static/css/main.d247de8a.chunk.css"
+  },
+  {
+    "revision": "5dbead62dd3cf68be35a",
+    "url": "/Portfolio-Website/static/js/2.1db9c9fb.chunk.js"
+  },
+  {
+    "revision": "b4be63e519f388173e2f4209d9f0a6dd",
+    "url": "/Portfolio-Website/static/js/2.1db9c9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b603c3bdab12a979d10b",
+    "url": "/Portfolio-Website/static/js/main.9676e317.chunk.js"
+  },
+  {
+    "revision": "8a549b7f018bd7134db0",
+    "url": "/Portfolio-Website/static/js/runtime-main.b90d5766.js"
+  }
+]);
